9e889a1b6bc31b8f19c7af80029ad0c489bc8bbb
Matheus <matheussalles150903@gmail.com>
2019-03-08 12:08:52 -0300

    Primeira versão teste

---
+++ Desktop/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva seu nome");
+           string name = Console.ReadLine();
+            Console.WriteLine("Ah ola {0}",name);
+        }
+    }
+}
